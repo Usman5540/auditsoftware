b42f0d7de15184e88fae4024b78a6f2d6b94f31e
Muhammad Usman <128804456+Usman5540@users.noreply.github.com>
2024-10-03 10:32:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,35 +4,39 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+
 // Load environment variables
 dotenv.config();
 
 // Create Express app
 const app = express();
 
-// app.use(cors({
-//     origin: '*', // Allows all origins
-//     methods: ['GET', 'POST', 'DELETE', 'PUT'], // Allowed methods
-//     allowedHeaders: ['Content-Type']
-// }));
-app.use(cors()); 
+// Enable CORS for all requests
+app.use(cors());
+
 // Middleware to parse JSON
 app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Server running!');
 });
 
-console.log(process.env.JWT_SECRET)
 // Routes
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
+
 // MongoDB connection
 mongoose
-    .connect(process.env.MONGOO_URI)
-    .then(() => console.log('DB connection successful'));
+  .connect(process.env.MONGO_URI, {
+  
+  })
+  .then(() => console.log('DB connection successful'))
+  .catch((err) => {
+    console.error('DB connection error:', err.message);
+  });
 
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
