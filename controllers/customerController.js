84d4d760b82b6929c9ca1903bf92ac5f3ca53f2a
Muhammad Usman <128804456+Usman5540@users.noreply.github.com>
2024-10-04 03:15:55 -0700

    Update customerController.js

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -95,11 +95,11 @@ exports.getCustomerCount = async (req, res) => {
 exports.getCustomerList = async (req, res) => {
     try {
         // Selecting specific fields based on the user schema
-        const customers = await User.find().select('userDetails.logoPicture userDetails.company userDetails.contactPerson mobileNumber _id');
+        const customers = await User.find().select(' userDetails.company userDetails.contactPerson mobileNumber _id');
         const customerList = customers.map((customer, index) => ({
             sr: index + 1,
             id: customer._id, // Adding the customer ID
-            logo: customer.userDetails.logoPicture,
+            // logo: customer.userDetails.logoPicture,
             company: customer.userDetails.company,
             person: customer.userDetails.contactPerson,
             phone: customer.mobileNumber
